8b402d22beb5e8b3644847d8fbcb0a6f64f19014
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-02-17 09:42:34 +0100

    ny tilføjelse

---
+++ details.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let params = new URLSearchParams(window.location.search);
+    let id = params.get("id");
+
+    if (id) {
+        fetch(`data/${id}.json`)
+            .then(response => response.json())
+            .then(data => displayDestinationDetails(data))
+            .catch(error => console.error("Fejl ved hentning af data:", error));
+    } else {
+        fetch("data/destinations.json")
+            .then(response => response.json())
+            .then(data => displayDestinationList(data.destinations))
+            .catch(error => console.error("Fejl ved hentning af data:", error));
+
+        populateApartments();
+    }
+});
+
+function displayDestinationList(destinations) {
+    const container = document.getElementById("destination-list");
+    container.innerHTML = "";
+
+    destinations.forEach(dest => {
+        const item = document.createElement("div");
+        item.classList.add("box");
+        item.innerHTML = `
+            <img src="img/${dest.image}" alt="${dest.name}" class="destination-img">
+            <div class="content">
+                <h3>${dest.destination}</h3>
+                <h1>${dest.title}</h1>
+                <h4>${dest.subtitle}</h4>
+                <p>${dest.text}</p>
+                <ul><li>${dest.facilities}<li></ul>
+
+            </div>
+            </div>
+        `;
+        container.appendChild(item);
+    });
+}
+
+
+
+function populateApartments() {
+    const grid = document.getElementById("apartment-grid");
+    grid.innerHTML = "";
+
+    const apartments = [
+        { image: "img/apartment1.jpg", title: "Apartment 1" },
+        { image: "img/apartment2.jpg", title: "Apartment 2" },
+        { image: "img/apartment3.jpg", title: "Apartment 3" },
+        { image: "img/apartment4.jpg", title: "Apartment 4" }
+    ];
+
+    apartments.forEach(apartment => {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.innerHTML = `
+            <img src="${apartment.image}" alt="${apartment.title}" class="apartment-img">
+            <div class="content">
+                <p>${apartment.destination}</p>
+
+            </div>
+        `;
+        grid.appendChild(box);
+    });
+}
